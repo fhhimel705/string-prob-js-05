4e8b1bd8219539714b1cbd9a7f72889341f73b90
FH Himel <fhhimel17@gmail.com>
2023-12-18 00:39:29 +0600

    added-final

--- test-02.js
+++ test-02.js
@@ -1,3 +1,7 @@
-const concat = 'hello';
-concat = concat + 'world!';
-console.log(concat);
+// const concat = 'hello';
+// concat = concat + 'world!';
+// console.log(concat);
+
+let concat02 = 'hello';
+concat02 = concat02 + 'world!';
+console.log(concat02);
